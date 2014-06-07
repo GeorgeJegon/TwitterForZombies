15a8c817f7cbf7cbcc0c74cf6ce1ebe96764bbfc
George Jegon <george_jegon@hotmail.com>
2014-06-07 01:24:01 -0300

    Include Banner slideshow with jQuery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.slides.min
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
--- app/assets/javascripts/banners.js
+++ app/assets/javascripts/banners.js
@@ -1,2 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function(){
+  $(".slideshow").slidesjs({
+    width: 960,
+    height: 300,
+    navigation: {
+      active: true
+    },
+    pagination: {
+      active: true
+    }
+  });
+});
