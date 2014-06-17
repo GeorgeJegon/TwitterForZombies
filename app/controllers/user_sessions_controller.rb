35f7885affc234cf314904c248431f4f80bc15b0
George Jegon <george_jegon@hotmail.com>
2014-06-17 18:39:06 -0300

    Removing error on UserSession logic

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,6 +1,6 @@
 class UserSessionsController < ApplicationController
-  before_action :require_no_authentication, only: [:new, :create]
-  before_action :require_authentication, only: :destroy
+  #efore_action :require_no_authentication, only: [:new, :create]
+  #before_action :require_authentication, only: :destroy
 
   def new
     @user_session = UserSession.new(session)
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,5 @@ class User < ActiveRecord::Base
   has_secure_password
 
   def self.authenticate(email, password)
-    user = find_by(email: email).try(:authenticate, password)
   end
-
 end
